fc0eae80c8fd9b946e20839316e650bbd14d1214
Benjamin Joseph <benejoseph@gmail.com>
2024-12-29 14:52:51 -0800

    trying out linked lists

---
+++ too_many_lists/target/debug/deps/too_many_lists-6166484052e6b21c.d
@@ -0,0 +1,8 @@
+/home/benejoseph/dev/rust/too_many_lists/target/debug/deps/libtoo_many_lists-6166484052e6b21c.rmeta: src/lib.rs src/first.rs
+
+/home/benejoseph/dev/rust/too_many_lists/target/debug/deps/libtoo_many_lists-6166484052e6b21c.rlib: src/lib.rs src/first.rs
+
+/home/benejoseph/dev/rust/too_many_lists/target/debug/deps/too_many_lists-6166484052e6b21c.d: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
